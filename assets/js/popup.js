59f2900d65880823b1e55e4bdbaab16999f3d6ab
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-11-17 23:55:50 +0200

    Warn popup update, Shiki tiktok

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,9 @@
-// Get the modal
-var modal = document.getElementById('id01');
+function warn(link) {
+  document.getElementById("warn_url").href=`${link}`; 
+  return false;
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+function dismissWarn() {
+  $('#warnModal').modal('hide');
+  return false;
+};
