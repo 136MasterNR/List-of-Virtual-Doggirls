4c8c8757e761f8836781bb23a98a50a2dca0add4
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-11-20 19:12:53 +0200

    Experimental Update: Profiles

---
+++ assets/js/querystrings.js
@@ -0,0 +1,18 @@
+/* experiment */
+
+let url = new URL(window.location);
+let searchParams = new URLSearchParams(url.search);
+
+
+if (searchParams.get('theme') === "dark") {
+    localStorage.setItem('theme', 'dark');
+} else if (searchParams.get('theme') === "white") {
+    localStorage.setItem('theme', 'white');
+}
+
+let theme=window.localStorage.theme
+
+
+if (searchParams.get('localstorage') === "clear") {
+    localStorage.clear();
+}
---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+/* Enable Tooltips */ $('[data-toggle="tooltip"]').tooltip()
+
+/* Copy Function */
+function copy(link) {
+    navigator.clipboard.writeText(link);
+}
+
+/* Incopatibilty Changes for Experimental Updates */
+
+if (window.innerWidth < 1100) {
+    document.getElementById('list_pochi_wanmaru').onclick=null
+    document.getElementById('list_pochi_wanmaru').href="https://www.youtube.com/channel/UCP0csSl19lOSNwJGCFsJqAQ"
+}
