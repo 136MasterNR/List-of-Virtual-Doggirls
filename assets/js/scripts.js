b08331efaed46b4d632344c247bfdf92326f8fe5
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-11-21 16:25:33 +0200

    Profiles compatible on mobile.
    idk what i am doing anymore, please don't look at the css...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,9 +5,11 @@ function copy(link) {
     navigator.clipboard.writeText(link);
 }
 
-/* Incopatibilty Changes for Experimental Updates */
+/* Incopatibilty Changes for Experimental Updates
 
 if (window.innerWidth < 1100) {
     document.getElementById('list_pochi_wanmaru').onclick=null
     document.getElementById('list_pochi_wanmaru').href="https://www.youtube.com/channel/UCP0csSl19lOSNwJGCFsJqAQ"
 }
+
+*/
