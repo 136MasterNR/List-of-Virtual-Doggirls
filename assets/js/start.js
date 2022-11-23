64a9a094882a14258b3e4979175329e43c10a059
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-11-23 18:22:14 +0200

    added spinning dog loading screen
    + updated jquery
    + fixed small background color issues

---
+++ assets/js/start.js
@@ -0,0 +1,15 @@
+const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+  (typeof elementOrSelector === 'string' ? document.querySelector(elementOrSelector) : elementOrSelector ).style.display = visible ? 'block' : 'none';
+
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#vtubers').style.display = "block"
+    document.querySelector('#vtubers').style.animation="onscreenRev 0.75s ease-out 1"
+    document.querySelector('#bottom').style.display = "block"
+    document.querySelector('#bottom').style.animation="onscreenRev 0.75s ease-out 1"
+    document.querySelector('#loading').style.animation="fadeOut 0.12s ease-out forwards 1"
+});
