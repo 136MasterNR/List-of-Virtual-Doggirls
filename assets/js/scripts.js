619606e3a7e77e60f1367acc7def9f20aefc3a88
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-11-23 20:29:21 +0200

    added profile links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,18 @@
 /* Enable Tooltips */ $('[data-toggle="tooltip"]').tooltip()
 
 /* Copy Function */
-function copy(link) {
+function copy(link,profile) {
     navigator.clipboard.writeText(link);
+    if (profile) {
+        $(document.querySelector(`#${profile} .profile_items .profile_text .title snap`)).tooltip('hide')
+            .attr('data-original-title', 'Copied!')
+            .tooltip('show');
+        setTimeout(
+            function() {
+                $(document.querySelector(`#${profile} .profile_items .profile_text .title snap`)).tooltip('hide')
+                    .attr('data-original-title', 'Copy Profile Link')
+        }, 2000);
+    }
 }
 
 /* Incopatibilty Changes for Experimental Updates
