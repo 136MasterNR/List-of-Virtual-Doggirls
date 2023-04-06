8530615fcaf174b634b0c2fc23c68010bf1dfda0
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-04-06 21:51:10 +0300

    secured emails to prevent email-harvesting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,9 @@ const copy = (profile) => {
         }, 2000);
     }
 }
+
+/* Secure Mails */
+const secure = (element, domain, at, event) => {
+    event.preventDefault();
+    element.outerHTML = `<a href="mailto:${domain}&#64;${at}&#46;com">${domain}&#64;${at}&#46;com</a> <i class="fa-solid fa-envelope fa-sm" aria-hidden="true"></i>`;
+}
