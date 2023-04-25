0fb023f7cc733f24b191536926d6a01350c03323
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-04-25 04:35:50 +0300

    search function! wow. added new dog Chibi! wow.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,3 +29,56 @@ const secure = (element, domain, at, event) => {
     event.preventDefault();
     element.outerHTML = `<a href="mailto:${domain}&#64;${at}&#46;com">${domain}&#64;${at}&#46;com</a> <i class="fa-solid fa-envelope fa-sm" aria-hidden="true"></i>`;
 }
+
+/* Search Features */
+const searchBox = document.getElementById('search-box');
+const vtuberDivs = document.querySelectorAll('.vtuber');
+const noResultsMessage = document.getElementById('no-results-message');
+
+searchBox.addEventListener('input', () => {
+  const searchTerm = searchBox.value.toLowerCase();
+  let hasResults = false;
+
+  vtuberDivs.forEach(vtuberDiv => {
+    const nameElement = vtuberDiv.querySelector('.text .name');
+    if (nameElement !== null) {
+      const name = nameElement.textContent.toLowerCase();
+      if (name.includes(searchTerm)) {
+        vtuberDiv.style.display = '';
+        hasResults = true;
+      } else {
+        vtuberDiv.style.display = 'none';
+      }
+    }
+  });
+
+  if (hasResults) {
+    noResultsMessage.style.display = 'none';
+  } else {
+    noResultsMessage.style.display = 'block';
+  }
+});
+
+const search = () => {
+  const searchContainer = document.querySelector('.webflow-style-input');
+  const vtubers = document.getElementById('vtubers');
+  if (searchContainer.style.display === 'flex') {
+    vtubers.style.transition = 'transform 0.5s ease';
+    vtubers.style.transform = 'translateY(-85px)';
+    setTimeout(() => {
+      searchContainer.style.display = 'none';
+      vtubers.style.transition = 'transform 0s';
+      vtubers.style.transform = 'translateY(0)';
+    }, 500);
+  } else {
+    searchContainer.style.display = 'flex';
+    vtubers.style.transition = 'transform 0s';
+    vtubers.style.transform = 'translateY(-85px)';
+    setTimeout(() => {
+      vtubers.style.transition = 'transform 0.5s ease';
+      vtubers.style.transform = 'translateY(0)';
+    }, 25);
+  }
+
+  searchBox.value = '';
+};
